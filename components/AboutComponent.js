8180747ecd8485190d08b4f61db9710c83e4d9a0
timstetter <tstetter@gmail.com>
2021-08-04 07:51:41 -0400

    animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Text, ScrollView } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { FlatList } from "react-native-gesture-handler";
+import * as Animatable from "react-native-animatable";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 
@@ -56,23 +57,27 @@ class About extends Component {
     if (this.props.partners.errMess) {
       return (
         <ScrollView>
-          <Mission />
-          <Card title="Community Partners">
-            <Text>{this.props.partners.errMess}</Text>
-          </Card>
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Mission />
+            <Card title="Community Partners">
+              <Text>{this.props.partners.errMess}</Text>
+            </Card>
+          </Animatable.View>
         </ScrollView>
       );
     }
     return (
       <ScrollView>
-        <Mission />
-        <Card title={"Community Partners"}>
-          <FlatList
-            data={this.props.partners.partners}
-            renderItem={renderPartner}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card title="Community Partners">
+            <FlatList
+              data={this.props.partners.partners}
+              renderItem={renderPartner}
+              keyExtractor={(item) => item.id.toString()}
+            />
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
